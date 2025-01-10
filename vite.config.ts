2f3063ac2ef3c104f55c392f5cc78d6f7fb44812
klsklnPITX <kilian.kalus@pure-it-worx.com>
2025-01-10 14:26:52 +0100

    added yaml for pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/algo-rewards-frontend/",
+  build: {
+    outDir: "build", // Ensure this matches your YAML configuration
+  },
   plugins: [react()],
-})
+});
